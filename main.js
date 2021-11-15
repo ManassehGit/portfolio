686ab445770b10d0250c8a046a92e9d772fb0966
Manasseh <manasseh.sarfo@amalitech.org>
2021-11-14 16:33:41 -0800

    First portfolio release update 3

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 let nav = document.querySelector('#nav');
-let main = document.querySelector('main');
 
 function changeBG(){
     if(window.scrollY > 0){
         // nav.style.backgroundColor = "white";
-        alert('hi');
     }
 }
 
-window.onscroll = changeBG;
+// window.onscroll = changeBG;
